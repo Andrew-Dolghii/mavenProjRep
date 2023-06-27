6945e6964f2efce7db4c37b2e8aea112821ce219
Andrew <andrew.dolghii@gmail.com>
2023-06-27 17:12:07 -0400

    innit commit

---
+++ src/main/java/org/andrewSpring/Music.java
@@ -0,0 +1,5 @@
+package org.andrewSpring;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/org/andrewSpring/TestSpring.java
@@ -0,0 +1,36 @@
+package org.andrewSpring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+//        Music music = context.getBean("rockMusic", Music.class);
+//
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//
+//        classicalMusicPlayer.playMusic();
+
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+//        Computer computer = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        context.close();
+    }
+}
